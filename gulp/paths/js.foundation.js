e2b40a8c92bb9e249e4c5f8ba1354ce931154e88
Ion Talmaci <ntalmaci171@gmail.com>
2018-11-29 15:14:53 +0200

    preparing the work space

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = [
-  './node_modules/jquery/dist/jquery.min.js'
+  './node_modules/jquery/dist/jquery.min.js',
+  './source/js/what-input.js',
+  './source/js/foundation.min.js'
 ];
